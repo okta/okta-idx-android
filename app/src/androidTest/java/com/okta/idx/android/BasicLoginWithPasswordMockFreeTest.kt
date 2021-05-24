4bd9454e014b7ac8c369adc3714da65f52bb99f4
Nikita <nikita.avraimov@okta.com>
2021-05-24 10:51:09 +0300

    scenario 1.1.2

--- app/src/androidTest/java/com/okta/idx/android/BasicLoginWithPasswordMockFreeTest.kt
+++ app/src/androidTest/java/com/okta/idx/android/BasicLoginWithPasswordMockFreeTest.kt
@@ -17,8 +17,10 @@ package com.okta.idx.android
 
 import androidx.lifecycle.Lifecycle
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -33,9 +35,24 @@ class BasicLoginWithPasswordMockFreeTest {
 
     companion object {
         private const val FORGOT_PASSWORD_BUTTON = "com.okta.idx.android:id/forgot_password_button"
+        private const val ERROR_TEXT_VIEW = "com.okta.idx.android:id/error_text_view"
+        private const val USERNAME_EDIT_TEXT = "com.okta.idx.android:id/username_edit_text"
     }
 
     @get:Rule val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test fun scenario_1_1_2_Mary_doesn_t_know_her_username() {
+        activityRule.scenario.moveToState(Lifecycle.State.RESUMED)
+        onView(withId(R.id.login_button)).perform(click())
+        waitForElement(USERNAME_EDIT_TEXT)
+
+        onView(withId(R.id.username_edit_text)).perform(ViewActions.replaceText("mary@unknown.com"))
+        onView(withId(R.id.password_edit_text)).perform(ViewActions.replaceText("superSecret"))
+        onView(withId(R.id.submit_button)).perform(click())
+
+        waitForElement(ERROR_TEXT_VIEW)
+        onView(withId(R.id.error_text_view)).check(matches(ViewMatchers.withText("You do not have permission to perform the requested action.")))
+    }
 
     @Test fun scenario_1_1_8_Mary_clicks_on_the_forgot_password_link() {
         activityRule.scenario.moveToState(Lifecycle.State.RESUMED)
