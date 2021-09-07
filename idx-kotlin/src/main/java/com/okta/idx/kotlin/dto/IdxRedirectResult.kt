d17dd1645c2f33113c13e8cd973de2e608a59d53
Jay Newstrom <jaynewstrom@gmail.com>
2021-09-07 11:04:46 -0500

    More documentation.

--- idx-kotlin/src/main/java/com/okta/idx/kotlin/dto/IdxRedirectResult.kt
+++ idx-kotlin/src/main/java/com/okta/idx/kotlin/dto/IdxRedirectResult.kt
@@ -25,6 +25,7 @@ sealed class IdxRedirectResult {
      * The redirect resulted in a successful authentication, which concluded with tokens.
      */
     data class Tokens internal constructor(
+        /** The token response. */
         val response: TokenResponse,
     ) : IdxRedirectResult()
 
@@ -33,6 +34,7 @@ sealed class IdxRedirectResult {
      * interaction from the user.
      */
     data class InteractionRequired internal constructor(
+        /** The response representing the current state for the authentication transaction. */
         val response: IdxResponse,
     ) : IdxRedirectResult()
 
@@ -40,7 +42,9 @@ sealed class IdxRedirectResult {
      * There was an error when attempting to handle the redirect.
      */
     data class Error internal constructor(
+        /** The error message associated with the error. */
         val errorMessage: String,
+        /** The optional exception associated with the error. */
         val exception: Exception? = null,
     ) : IdxRedirectResult()
 }
