613b4b4609b3eb6267b140095a167766662a1e23
Jay Newstrom <jaynewstrom@gmail.com>
2022-03-29 12:10:19 -0500

    Run E2E tests.

--- signInToGcloud.sh
+++ signInToGcloud.sh
@@ -2,7 +2,7 @@
 
 export CLOUDSDK_CORE_DISABLE_PROMPTS=1
 curl https://sdk.cloud.google.com | bash > /dev/null
-echo echo $GOOGLE_SERVICE_JSON_BASE64 | base64 --decode > client-secret.json
+echo $GOOGLE_SERVICE_JSON_BASE64 | base64 --decode > client-secret.json
 gcloud auth activate-service-account --key-file client-secret.json
 source $HOME/google-cloud-sdk/path.bash.inc
 gcloud config set project $GOOGLE_PROJECT_ID
